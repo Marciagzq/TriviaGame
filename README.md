8161cf354bc09759d7aeafd3c9bbda6169c0134c
Marcia Zegarra <marcia.gzq@gmail.com>
2019-07-10 16:41:17 -0400

    Ready for the world!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TriviaGame
 Soccer Trivia!
+
+Hello Everybody!
+
+The website presented is a Soccer Trivia, you will have time to proof some of the most basic but important facts of the Soccer world.
+At the beggining of the game, you will have an alert as an introduction, basically whenever you decided to click "Ok" the game will start and from there, you will have 2 minutes to choose the answers you think are correct by clicking the respective button.
+Once you run out of time, The page will display your score. 
+You may be able to re-start the game by refreshing the page (F5 or on refresh button on the top of the page.)
+
+Contributor:
+All the user can propose new information or suggest any channge by sending me a email: marcia.gzq@gmail.com, every opinion or suggestion is very important for me as a beginner developer and crucial for my proffesional growth. 
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,14 @@
-//
-var timerNumber = 5;
+//Simple welcome alert
+alert("Ready to proof how much do you know about the World Cups?");
+
+//Time variables 
+var timerNumber = 120;
 var converted = timeConverter(timerNumber);
 var setIntervalHandler;
 
-//
 
+
+//this function displays the time with minutes and seconds, exactly like a clock
 function timeConverter(t) {
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
@@ -17,7 +21,7 @@ function timeConverter(t) {
     } return minutes + ":" + seconds;
 }
 
-//
+//this function helps decrementing the time per second
 function decrement() {
     timerNumber--
     if (timerNumber == 0) {
@@ -25,7 +29,7 @@ function decrement() {
     }
 }
 
-//
+// This function is call whenever time has end and displays hidden answers elements & functions
 function timeStop() {
     clearInterval(setIntervalHandler);
     $("#container").hide();
@@ -38,9 +42,9 @@ function timeStop() {
     $("#correct-Answers").append(correct); 
     $("#incorrect-Answers").append(incorrect); 
     $("#unanswers").append(unanswer); 
-
 }
 
+//this functions checks and adds our answers wether correct, incorrect or unanswered.
 function answers(myClass) {
     var generalAnswers = 0;
     $(myClass).each(function () {
@@ -52,18 +56,21 @@ function answers(myClass) {
     return generalAnswers;
 }
 
+
 // *****************JQUERY*************************//
 $(document).ready(function () {
 
-    //
+    
     $("#cointainer-Answers").hide();
-
     $("#timer").text(converted);
     setIntervalHandler = setInterval(function () {
         decrement();
         $("#timer").text(timeConverter(timerNumber));
     }, 1000);
-
+    
+    $(".submit").click(function() {
+        timeStop();
+      });
 });
 
 
